8a9a59f1ff816b9309b6cdda72b1d0eb020ba5ed
Adriano Martins <adrianomartins@gmail.com>
2016-02-28 21:16:40 -0300

    Add Toast Animation (2/2)
    Change-Id: I43328f2236981e8afbccc7ee0133f9198656deb0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3483,6 +3483,12 @@ public final class Settings {
         public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
 
         /**
+         * Toast Animations
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+        
+        /**
          * Whether navigation bar is placed on the left side in landscape mode
          * @hide
          */
--- core/java/android/widget/Toast.java
+++ core/java/android/widget/Toast.java
@@ -35,6 +35,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityManager;
+import android.provider.Settings;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -418,6 +419,50 @@ public class Toast {
                 // the layout direction
                 final Configuration config = mView.getContext().getResources().getConfiguration();
                 final int gravity = Gravity.getAbsoluteGravity(mGravity, config.getLayoutDirection());
+               switch(Settings.System.getInt(context.getContentResolver(), Settings.System.TOAST_ANIMATION, 1)) {
+                case 0:
+                mParams.windowAnimations = -1;
+                break;
+                case 1:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast;
+                break;
+                case 2:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Fade;
+                break;
+                case 3:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideRight;
+                break;
+                case 4:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_SlideLeft;
+                break;
+                case 5:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Xylon;
+                break;
+                case 6:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Toko;
+                break;
+                case 7:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Tn;
+                break;
+                case 8:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Honami;
+                break;
+                case 9:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_FastFade;
+                break;
+                case 10:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFade;
+                break;
+                case 11:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFadeCenter;
+                break;
+                case 12:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_GrowFadeBottom;
+                break;
+                case 13:
+                mParams.windowAnimations = com.android.internal.R.style.Animation_Toast_Translucent;
+                break;
+                }
                 mParams.gravity = gravity;
                 if ((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.FILL_HORIZONTAL) {
                     mParams.horizontalWeight = 1.0f;

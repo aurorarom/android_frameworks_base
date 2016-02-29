cf2da5e8325379375022626ff8c3f4ebf3fe12e9
Adriano Martins <adrianomartins@gmail.com>
2016-02-28 22:22:21 -0300

    Add listview animations (1/2)
    Change-Id: I7e9a43f6f4616b34ba077d2d4fb7e2beedf27845

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3606,6 +3606,12 @@ public final class Settings {
         public static final String STATUSBAR_COMPONENT = "statusbar_component";
 
         /**
+         * Toast animations
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+        
+        /**
          * Custom System Animations
          *
          * @hide
@@ -3628,10 +3634,33 @@ public final class Settings {
         public static final String ANIMATION_CONTROLS_REVERSE_EXIT = "animation_controls_reverse_exit";
         
         /**
-         * Toast animations
+         * ListView Animations
+         * 0 == None
+         * 1 == Wave (Left)
+         * 2 == Wave (Right)
+         * 3 == Scale
+         * 4 == Alpha
+         * 5 == Stack (Top)
+         * 6 == Stack (Bottom)
+         * 7 == Translate (Left)
+         * 8 == Translate (Right)
          * @hide
          */
-         public static final String TOAST_ANIMATION = "toast_animation";
+        public static final String LISTVIEW_ANIMATION = "listview_animation";
+        
+        /**
+         * ListView Interpolators
+         * 0 == None
+         * 1 == accelerate_interpolator
+         * 2 == decelerate_interpolator
+         * 3 == accelerate_decelerate_interpolator
+         * 4 == anticipate_interpolator
+         * 5 == overshoot_interpolator
+         * 6 == anticipate_overshoot_interpolator
+         * 7 == bounce_interpolator
+         * @hide
+         */
+        public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
   
         /**
          * Settings to backup. This is here so that it's in the same place as the settings

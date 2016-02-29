4d22b4b6d2b452443b48050be4093dc54734b48c
Adriano Martins <adrianomartins@gmail.com>
2016-02-28 21:04:43 -0300

    Revert "Add Toast Animation (2/2)"
    This reverts commit ef425b21fbbb0e787b629b9fcdd3f5463943823f.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3568,13 +3568,7 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_COMPONENT = "statusbar_component";
-        
-        /**
-         * Toast animations
-         * @hide
-         */
-         public static final String TOAST_ANIMATION = "toast_animation";
-  
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.

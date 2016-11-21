7c9a15c75e305097b98ce42b363f4fd23a2b5d98
Adriano Martins <adrianomartins@gmail.com>
2016-11-21 01:28:18 -0200

    Revert "zygote: Allow device to append extra whitelisted paths"
    This reverts commit cf70929d108d86d86d166d6aa21f388544d2685f.

--- core/jni/fd_utils-inl.h
+++ core/jni/fd_utils-inl.h
@@ -35,8 +35,6 @@
 #include "JNIHelp.h"
 #include "ScopedPrimitiveArray.h"
 
-#include <fd_utils-inl-extra.h>
-
 // Whitelist of open paths that the zygote is allowed to keep open.
 //
 // In addition to the paths listed here, all files ending with
@@ -62,9 +60,6 @@ static const char* kPathWhitelist[] = {
   "/dev/ion",
   "@netlink@",
   "/system/framework/org.cyanogenmod.platform-res.apk",
-#ifdef PATH_WHITELIST_EXTRA_H
-PATH_WHITELIST_EXTRA_H
-#endif
 };
 
 static const char* kFdPath = "/proc/self/fd";
